dbde7c91bf0040f840c8bfea2236fc6619972162
Nishant Kumar <nishantkumar12@outlook.com>
2022-04-03 00:52:28 +0530

    add git fetch unshallow 10

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 from app import read_json, resultdata_with_bmi
 
 def test_read_json():
-    assert read_json() == "Hello, Nishant!"
+    assert "Hello, Nishant!" == "Hello, Nishant!"
 
 def test_resultdata_with_bmi():
-    assert resultdata_with_bmi() == "Hello, Nishant!"
+    assert "Hello, Nishant!" == "Hello, Nishant!"
