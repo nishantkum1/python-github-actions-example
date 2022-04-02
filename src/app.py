8d25cf379967a86b6ffb43e6fb89b50039e8de31
Nishant Kumar <nishantkumar12@outlook.com>
2022-04-03 00:48:54 +0530

    add git fetch unshallow 9

--- src/app.py
+++ src/app.py
@@ -1,10 +1,116 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello, Nishant!"
+# @app.route("/")
+# def index():
+#     return "Hello, Nishant!"
 
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+#     app.run()
+
+# Python program to calculate BMI
+
+import json
+import csv
+
+#BMI category class for Table 1
+class BMI:
+    bmi_categoty ={ 'UW' :'Underweight',
+                    'NW': 'Normal Weight',
+                    'OW': 'Over Weight',
+                    'MO': 'Moderately obese',
+                    'SO': 'Severly obese',
+                    'VSO': 'Very Severly obese',
+                   }
+
+    health_risk = {
+                    'MR':'Malnutrition Risk',
+                    'LR': 'Low Risk',
+                    'ER': 'Enhanced Risk',
+                    'MRS': 'Medium Risk',
+                    'HR': 'High Risk',
+                    'VHR': 'Very High Risk',
+    }
+
+#Read input json file
+def read_json():
+    list_of_row = []
+    with open('input.json') as file:
+        alllines = json.load(file)
+        for row in alllines:
+            list_of_row.append(row)
+            #print(row)
+        return list_of_row    
+
+def resultdata_with_bmi(alldata):
+    line_count = 0
+    notValidData = []
+    validData = []    
+    for row in alldata:
+        if line_count == 0:
+            line_count = 1
+        elif row["Gender"].isalpha() == True and str(row["HeightCm"]).isnumeric() == True and str(row["WeightKg"]).isnumeric() == True :
+            bmi = calculate_bmi(row)
+            validData.append(list(row.values())+bmi)
+            line_count +=1
+        else:
+            notValidData.append(list(row.values()))
+            line_count +=1
+    #print('this data is not valid-->',notValidData)
+    #print('total_rows-->',line_count)
+    return validData
+
+#Calculate BMI
+def calculate_bmi(data):
+    BMI_categoty= None
+    Health_risk= None
+    BMI_range = None
+    x =int(data["HeightCm"])
+    y = int(data["WeightKg"])
+    bmi = y/((x/100)**2)
+    if bmi <= 18.4 :
+        BMI_categoty = BMI.bmi_categoty.get('UW')
+        BMI_range = bmi
+        Health_risk = BMI.health_risk.get('MR')
+    elif bmi >=18.5 and bmi <= 24.9:
+        BMI_categoty = BMI.bmi_categoty.get('NW')
+        BMI_range = bmi
+        Health_risk = BMI.health_risk.get('LR')
+    elif bmi >=25 and bmi <= 29.9:
+        BMI_categoty = BMI.bmi_categoty.get("OW")
+        BMI_range = bmi
+        Health_risk = BMI.health_risk.get('ER')
+    elif bmi >=30 and bmi <= 34.9:
+        BMI_categoty = BMI.bmi_categoty.get("MO")
+        BMI_range = bmi
+        Health_risk = BMI.health_risk.get('MRS')
+    elif bmi >= 35 and bmi <= 39.9:
+        BMI_categoty = BMI.bmi_categoty.get("SO")
+        BMI_range = bmi
+        Health_risk = BMI.health_risk.get('HR')
+    elif bmi > 40:
+        BMI_categoty = BMI.bmi_categoty.get("VSO")
+        BMI_range = bmi
+        Health_risk = BMI.health_risk.get('VHR')
+    bmi_list =[BMI_categoty,BMI_range,Health_risk]
+    return bmi_list
+
+
+def write_csv(data):
+    with open('output_for_BMI_Calculator_Height_Weight.csv', 'w',newline='') as file:
+        csv_data = csv.writer(file, delimiter=',')
+        csv_data.writerow(['Gender', 'Height', 'Weight', 'BMI_categoty', 'BMI_range', 'Health_risk'])
+        for row in data:
+            # print(row)
+            csv_data.writerows([row])
+    return 'csv file added successfully..'
+
+
+if __name__ == '__main__':
+    contents = read_json()
+    #print(contents)
+    finaldata = resultdata_with_bmi(contents)
+    print('final data with BMI-->',finaldata)
+    #result = write_csv(finaldata)
+    #print(result)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,7 @@
-from app import index
+from app import read_json, resultdata_with_bmi
 
-def test_index():
-    assert index() == "Hello, Nishant!"
+def test_read_json():
+    assert read_json() == "Hello, Nishant!"
+
+def test_resultdata_with_bmi():
+    assert resultdata_with_bmi() == "Hello, Nishant!"
